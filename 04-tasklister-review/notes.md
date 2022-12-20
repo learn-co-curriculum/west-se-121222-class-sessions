aeaa9956124da5650e60f8e75f70ed2fa309cd1d
betalantz <betalantz@gmail.com>
2022-12-19 16:24:29 -0800

    add solution to 04

---
+++ 04-tasklister-review/notes.md
@@ -0,0 +1,22 @@
+### Deliverables
+
+[x] As a user, I should be able to type a task into the input field.
+[x] a user, I should be able to click some form of a submit button.
+[x] As a user, I expect to see the task string that I provided appear in the DOM
+  after the submit button has been activated.
+
+### Stretch Deliverables
+
+Once you've got the required deliverables working, you may want to try to
+implement one or more of the following:
+
+[x] A delete function that will remove tasks from your list
+* A priority value selected from a [dropdown][] that is used to determine the color
+  of the text in the list (e.g. red for high priority, yellow for medium, green
+  for low)
+  * As an additional challenge, implement a sorting functionality that displays
+    the tasks in ascending or descending order based on priority
+* An additional input field (e.g. user, duration, date due)
+[x] Ability to edit tasks
+* Something of your choice! The main objective is to add a feature that allows
+  the user's input to affect the DOM
--- 04-tasklister-review/src/index.js
+++ 04-tasklister-review/src/index.js
@@ -1,7 +1,68 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code goes here
-  
-});
+// DOM Selectors
+const form = document.querySelector('#create-task-form')
+const taskList = document.querySelector('#tasks') // anchor element
+const description = document.querySelector('#description')
+
+// Event listners
+form.addEventListener('submit', addTask)
+
+// Event handlers
+function addTask(event){
+  // "get data"
+  event.preventDefault()
+  console.log('description: ', description.value);
+  console.log(event.target.description.value)
+  renderTask(event.target.description.value)
+  form.reset()
+} 
+
+function editTask(e, li){
+  e.preventDefault()
+  // console.dir(li)
+  const deleteBtn = li.querySelector(':nth-child(1)')
+  const editBtn = li.querySelector(':nth-child(2)')
+  console.log('deleteBtn: ', deleteBtn);
+  li.textContent = e.target.editTask.value
+  li.append(deleteBtn, editBtn)
+}
+
+// Render functions
+
+function renderTask(taskString){
+  // element creations
+  const newTask = document.createElement('li')
+  const deleteBtn = document.createElement('button')
+  const editBtn = document.createElement('button')
+  // assign values and attributes
+  newTask.textContent = taskString
+  deleteBtn.textContent = 'Delete'
+  deleteBtn.style.color = 'red'
+  editBtn.textContent = "Edit"
+  //appending
+  newTask.append(deleteBtn, editBtn)
+  taskList.append(newTask)
+
+  // event listeners
+  deleteBtn.addEventListener('click', () => newTask.remove())
+  editBtn.addEventListener('click', () => renderEditForm(newTask, taskString))
+}
+
+function renderEditForm(li, taskstring){
+  console.log('li: ', li);
+  const editForm = document.createElement('form')
+  const inputField = document.createElement('input')
+  const submitBtn = document.createElement('button')
+
+  inputField.type = 'text'
+  inputField.name = 'editTask'
+  inputField.placeholder = taskstring
+  submitBtn.type = 'submit'
+  submitBtn.textContent = 'Change'
+
+  editForm.append(inputField, submitBtn)
+  li.append(editForm)
+
+  editForm.addEventListener('submit', (e) => editTask(e, li))
+}
 
 
-q
