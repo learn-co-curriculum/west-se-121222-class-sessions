f1e2209eb7c298a5306cedfb89ceb62928a52850
betalantz <betalantz@gmail.com>
2023-01-12 14:59:22 -0800

    complete 2 palindrome solutions

--- algorithm-club/phase-1-algorithms-palindrome/index.js
+++ algorithm-club/phase-1-algorithms-palindrome/index.js
@@ -1,9 +1,58 @@
+// function isPalindrome(word) {
+//   // if 'reversed' and 'word' are same
+//   //   return true
+//   // else
+//   //   return false
+//   return reverse(word) === word
+
+// }
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from beginning of word to the middle
+  // compare the letter we're iterating on to the corresponding letter from the end
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    let endIndex = word.length - 1 - startIndex
+    // if they don't match, return false
+    if (word[startIndex] !== word[endIndex]){
+      return false
+    }
+  }
+  // if we reach the middle and all match, return true
+  return true
+}
+
+
+function reverse(word) {
+  // initialize 'reversed' var as ""
+  let reversed = ""
+  
+  // iterate through the word and concatenate each char to 'reversed'
+  for (let i=0; i<word.length; i++) {
+    reversed = word[i] + reversed
+  }
+
+  return reversed
 }
 
 /* 
   Add your pseudocode here
+initialize 'reversed' var as ""
+
+iterate through the word and concatenate each char to 'reversed'
+
+if 'reversed' and 'word' are same
+  return true
+else
+  return false
+
+*/
+
+/*
+iterate from beginning of word to the middle
+compare the letter we're iterating on to the corresponding letter from the end
+if they don't match, return false
+if we reach the middle and all match, return true
+
 */
 
 /*
@@ -20,6 +69,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("deed"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("deer"));
 }
 
 module.exports = isPalindrome;
