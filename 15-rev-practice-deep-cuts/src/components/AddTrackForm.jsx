fb379f4ed475e3f199512853dfdfeba864a4e7c7
betalantz <betalantz@gmail.com>
2023-01-18 09:34:08 -0800

    complete core deliverables 15

--- 15-rev-practice-deep-cuts/src/components/AddTrackForm.jsx
+++ 15-rev-practice-deep-cuts/src/components/AddTrackForm.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function AddTrackForm() {
+const initialFormData = {
+  image: "",
+  title: "",
+  artist: "",
+  BPM: ""
+}
+
+function AddTrackForm({ onSubmitTrack }) {
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  function handleChange(e){
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    onSubmitTrack(formData)
+    setFormData(initialFormData)
+  }
+
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input value="" type="text" name="image" placeholder="Image URL"/>
-          <input value="" type="text" name="title" placeholder="title" />
-          <input value="" type="text" name="artist" placeholder="Artist" />
-          <input value="" type="number" name="BPM" placeholder="BPM" step="1.00" />
+          <input onChange={handleChange} value={formData.image} type="text" name="image" placeholder="Image URL"/>
+          <input onChange={handleChange} value={formData.title} type="text" name="title" placeholder="title" />
+          <input onChange={handleChange} value={formData.artist} type="text" name="artist" placeholder="Artist" />
+          <input onChange={handleChange} value={formData.BPM} type="number" name="BPM" placeholder="BPM" step="1.00" />
         </div>
         <input className="" type="submit" value="Add Track" />
       </form>
--- 15-rev-practice-deep-cuts/src/components/TracksPage.jsx
+++ 15-rev-practice-deep-cuts/src/components/TracksPage.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './Search'
 import AddTrackForm from './AddTrackForm'
 import TracksList from './TracksList'
 
 function TracksPage() {
 
+    const baseUrl = "http://localhost:8001/tracks"
+
+    const [tracks, setTracks] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
+
+    useEffect(() => {
+      fetch(baseUrl)
+        .then(res => res.json())
+        .then(setTracks)
+    }, [])
+
+    const filteredTracks = tracks.filter(track => {
+      return track.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            track.artist.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            track.BPM.toString().includes(searchTerm)
+    })
+
+    function postTrack(newTrack){
+      const newTrackBody = {
+        ...newTrack,
+        BPM: Number(newTrack.BPM)
+      }
+      const options = {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTrackBody)
+      }
+      return fetch(baseUrl, options)
+        .then(res => res.json())
+    }
+
+    function addTrack(newTrack){
+      postTrack(newTrack)
+        .then(newTrack => setTracks([...tracks, newTrack]))
+    }
     
   return (
     <div>
-      <Search />
-      <AddTrackForm />
-      <TracksList />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <AddTrackForm onSubmitTrack={addTrack}/>
+      <TracksList tracks={filteredTracks}/>
     </div>
   )
 }
