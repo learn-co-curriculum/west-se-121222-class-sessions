2391f0b88a76dcf80ddec93aa927973f692f27fe
betalantz <betalantz@gmail.com>
2023-01-18 15:14:25 -0800

    compete deliv 1

--- 16-rev-practice-plantsy/src/components/PlantList.js
+++ 16-rev-practice-plantsy/src/components/PlantList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants }) {
+  console.log("🚀 ~ file: PlantList.js:5 ~ PlantList ~ plants", plants)
+
+  const plantCards = plants.map(plant => <PlantCard key={plant.id} {...plant}/>)
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantCards}</ul>
   );
 }
 
--- 16-rev-practice-plantsy/src/components/PlantPage.js
+++ 16-rev-practice-plantsy/src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantsArr, setPlantsArr] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(setPlantsArr)
+    
+  }, [])
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plantsArr}/>
     </main>
   );
 }
