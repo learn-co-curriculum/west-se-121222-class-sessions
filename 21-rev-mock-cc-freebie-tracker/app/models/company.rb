45d8e2f9d425869e93304fadbdbb6fdeba3d813f
betalantz <betalantz@gmail.com>
2023-02-08 15:11:52 -0800

    complete Company methods

--- 21-rev-mock-cc-freebie-tracker/app/models/company.rb
+++ 21-rev-mock-cc-freebie-tracker/app/models/company.rb
@@ -1,4 +1,25 @@
 class Company < ActiveRecord::Base
+
+    def self.oldest_company
+        # self.all.min_by {|company| company.founding_year} # vanilla Ruby
+        # self.all.min_by(&:founding_year)
+        # ======
+        # oldest_year = Company.minimum(:founding_year)
+        # Company.find_by(founding_year: oldest_year)
+        # ======
+        self.order(:founding_year).first
+    end
+
     has_many :freebies
     has_many :devs, through: :freebies
+
+    def give_freebie(dev, item_name, value)
+        # Freebie.create(
+        #     dev: dev,
+        #     company: self,
+        #     item_name: item_name,
+        #     value: value
+        # )
+        self.freebies.create(dev: dev, item_name: item_name, value: value)
+    end 
 end
