cb0137005f54e8b153f58e6294c0cf05a8944e6d
betalantz <betalantz@gmail.com>
2023-02-08 16:06:44 -0800

    complete all delivs plus extra

--- 21-rev-mock-cc-freebie-tracker/app/models/dev.rb
+++ 21-rev-mock-cc-freebie-tracker/app/models/dev.rb
@@ -1,4 +1,9 @@
 class Dev < ActiveRecord::Base
+
+    def self.dev_with_highest_value_of_freebies # not in deliverables
+        self.all.max_by(&:total_freebies_value)
+    end
+
     has_many :freebies
     has_many :companies, through: :freebies
 
@@ -10,4 +15,18 @@ class Dev < ActiveRecord::Base
     end
 
     def give_away(dev, freebie)
+        # if freebie.dev != self
+        #     "Dev doesn't own this item!"
+        # else
+        #     # new_freebie = self.freebies.where(item_name: freebie.item_name).first
+        #     freebie.update(dev: dev)
+        #     dev.reload
+        # end
+        freebie.update(dev: dev) unless freebie.dev != self
+    end
+
+    def total_freebies_value # not in deliverables
+        self.freebies.sum(:value)
+    end
+
 end
