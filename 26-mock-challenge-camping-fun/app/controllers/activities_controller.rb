62f281ff8d056367f1f14ded366cadfb942fd402
betalantz <betalantz@gmail.com>
2023-03-01 15:36:58 -0800

    complete actions Camper/Activity controllers 26

--- 26-mock-challenge-camping-fun/app/controllers/activities_controller.rb
+++ 26-mock-challenge-camping-fun/app/controllers/activities_controller.rb
@@ -1,2 +1,12 @@
 class ActivitiesController < ApplicationController
+
+    def index
+        render json: Activity.all
+    end
+
+    def destroy
+        Activity.find(params[:id]).destroy
+        head :no_content
+    end
+    
 end
